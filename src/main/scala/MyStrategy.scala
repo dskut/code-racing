9720e52377cac0af1de5eeeb04874c4c4378d7b9
Denis Kutukov <mail@dskut.ru>
2015-11-21 00:21:41 +0300

    use nitro

--- src/main/scala/MyStrategy.scala
+++ src/main/scala/MyStrategy.scala
@@ -15,7 +15,9 @@ class MyStrategy extends Strategy {
         var nextWaypointX: Double = (self.nextWaypointX + 0.5D) * game.trackTileSize
         var nextWaypointY: Double = (self.nextWaypointY + 0.5D) * game.trackTileSize
 
-        log(world, s"x=${self.x} y=${self.y} x_wp=${self.nextWaypointX} y_wp=${self.nextWaypointY} x_wp_next=$nextWaypointX y_wp_next=$nextWaypointY tile_size=${game.trackTileSize}")
+        val distToWaypoint = self.distanceTo(nextWaypointX, nextWaypointY)
+        log(world, s"x=${self.x} y=${self.y} x_wp=${self.nextWaypointX} y_wp=${self.nextWaypointY}"
+            ++ s" x_wp_next=$nextWaypointX y_wp_next=$nextWaypointY dist_to_wp=$distToWaypoint")
 
         val cornerTileOffset: Double = 0.25D * game.trackTileSize
         move.enginePower = 0.75
@@ -34,12 +36,16 @@ class MyStrategy extends Strategy {
             nextWaypointY -= cornerTileOffset
         case _ =>
             move.enginePower = 1
+            if (distToWaypoint > 300) {
+                move.useNitro = true
+            }
+            
         }
 
         val angleToWaypoint: Double = self.angleTo(nextWaypointX, nextWaypointY)
-        val speedModule: Double = hypot(self.speedX, self.speedY)
         move.wheelTurn = angleToWaypoint * 32.0D / Pi
 
+        val speedModule: Double = hypot(self.speedX, self.speedY)
         if (speedModule * speedModule * abs(angleToWaypoint) > 2.5D * 2.5D * Pi) {
             move.brake = true
         }
